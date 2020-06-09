a865eacc80e39e9a2eb6700a9b9f1b7b176998e3
shhu21 <hu.sara21@gmail.com>
2020-06-09 04:21:54 -0700

    adds initial files and pseudo code

---
+++ js/script.js
@@ -0,0 +1,62 @@
+// PSEUDO
+// var questions = [question strings]
+// var choices = [[answer choices strings]]
+// var correctAns = [correct answer strings]
+// var timer
+
+// function setQuestion (index) {
+        // give the user the question
+            // display question from array questions
+            // for (choices[index].length) {
+                // display answer choices
+            // }
+// }
+
+// function checkAns (index, answer) {
+    // check user answer
+    // return bool
+// }
+
+// function displayCheck (bool) {
+    // if(bool) {
+        // display "correct" message
+        // return;
+    // }
+    // display "wrong" message
+// }
+
+// inputUser {
+    // input user info and store into high scores
+    // may need to do a sort if local storage doesn't handle it
+// }
+
+// high score back button
+// onclick goBack(object) {
+    // reload object (saved previous state of 'main' html)
+// }
+
+// onlick clearScores {
+    // clear high scores (clear array or clear local storage whichever is handling the score storage)
+// }
+
+// onclick highscore display function {
+    // may need to pause timer if in the middle of the quiz and then restart
+        // also may need to do a save state of the page (get main html (may not need to do a separate stop for the timer))
+    // display high scores
+// }
+
+// *main function calls all the helper functions
+// button.onclick = function start {
+    // start timer
+    // for (questions.length) {
+        // setQuestion(i)
+        // var answer = get user answer
+        // checkAns(i, ans)
+        // var ans = check user answer
+        // if(!ans) {
+                // decrease timer by 10 seconds
+        // }
+        // displayCheck(ans)
+    // }
+    // call form function
+// }
