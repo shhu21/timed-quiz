a2c1dc48a0aad14a8ae248e12abd733b85f493ad
shhu21 <hu.sara21@gmail.com>
2020-06-09 23:47:20 -0700

    adds save state, checkAns templates and finishes runQuestions functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,9 @@ var questions = [
     "String values must be enclosed within ______ when being assigned to variables.",
     "A very useful tool used during development and debugging for printing content ot the debugger is:"
 ]
-// var correctAns = [correct answer strings]
+// [correct answer strings]
+// TODO: add in the rest of the actual answer choices
+var correctAns = ["strings", "booleans", "alerts", "numbers", "numbers"];
 // answer choices
 // TODO: add in the rest of the actual answer choices
 var choices = [
@@ -21,12 +23,20 @@ var choices = [
 var offset = 0;
 // user score
 var score = 0;
+// question index
+var index = 0;
+// timer
+var timer;
+// if the user is in the middle of the quiz
+var ifOngoing = false;
+// save state of quiz
+var state = document;
 
 function updateTimer() {
-    setInterval(function setTimer() {
+    return setInterval(function setTimer() {
         // get timer.innerHTML.toParse() (change to int)
-        var timer = document.getElementById('timer');
-        var temp = timer.innerHTML;
+        var time = document.getElementById('timer');
+        var temp = time.innerHTML;
         var index = temp.indexOf(" ") + 1;
         var x = parseInt(temp.substr(index, temp.length));
         if(offset > 0) {
@@ -34,39 +44,51 @@ function updateTimer() {
             offset = 0;
         }
         x++;
-    timer.innerHTML = "Timer: " + x;
-    }, 1000) // needs 1000 because it runs in milliseconds
+    time.innerHTML = "Timer: " + x;
+    }, 1000); // needs 1000 because it runs in milliseconds
+}
+
+var checkAns = function (event) {
+    if(event.target.id == 'correct') {
+        // display correct
+    }
+    else {
+        // display wrong
+    }
+    
+    index++;
+    runQuestions();
 }
  
-function setQuestion (index) {
+function setQuestion () {
     // give the user the question
     var temp = document.querySelector('div');
     if(temp) {
         temp.remove();
     }
-    var question = document.getElementById('main-content');
+
+    var question = document.getElementsByTagName('h1')[0];
     // display question from array questions
     question.innerHTML = questions[index];
+    var ansContainer = document.createElement('div');
     for (var i = 0; i < choices[index].length; i++) {
-        var ansContainer = document.createElement('div');
-        console.log(ansContainer)
         var createBtn = document.createElement('button');
         createBtn.innerHTML = choices[index][i];
         createBtn.className = "btn";
 
+        if(choices[index][i] == correctAns[i]) {
+            createBtn.id = "correct";
+        }
+
         ansContainer.appendChild(createBtn);
-        document.getElementsByTagName('main')[0].appendChild(ansContainer);
     }
+    document.getElementById('main-content').appendChild(ansContainer);
 
+    var choiceBtns = document.getElementsByClassName('btn')
+    for(var i = 0; i < choiceBtns.length; i++){
+        choiceBtns[i].addEventListener('click', checkAns);
+    }
 }
-
-// function checkAns (index, answer) {
-    // if(correctAns[i] !== answer) {
-            // offset = 10;
-            // setTimer()
-    // }
-    // return bool
-// }
 
 // function displayCheck (bool) {
     // if(bool) {
@@ -83,32 +105,42 @@ function setQuestion (index) {
 
 // high score back button
 // onclick goBack(object) {
-    // reload object (saved previous state of 'main' html)
+//     reload object (saved previous state of 'main' html)
 // }
 
 // onlick clearScores {
     // clear high scores (clear array or clear local storage whichever is handling the score storage)
 // }
 
-// onclick highscore display function {
-    // may need to pause timer if in the middle of the quiz and then restart
-        // also may need to do a save state of the page (get main html (may not need to do a separate stop for the timer))
+function saveState() {
+    // pause timer if in the middle of the quiz and then restart
+    clearInterval(timer);
+    // save state of quiz
+    state = document;
+};
+
+var viewHighScores = function() {
+    if(ifOngoing) {
+        saveState();
+    }
     // display high scores
-// }
+}
 
-// *main function calls all the helper functions
-document.getElementById('start').addEventListener("click", function () {
-    updateTimer();
+function runQuestions() {
+    if(index >= questions.length) {
+        return;
+    }
+    setQuestion();
+}
 
-    // for (var i = 0; i < questions.length; i++) {
-        setQuestion(0);
-        // var answer = get user answer
-        // checkAns(i, ans)
-        // var ans = check user answer
-        // if(!ans) {
-        //         decrease timer by 10 seconds
-        // }
-        // displayCheck(ans)
-    // }
+// starts quiz
+var startQuiz = function () {
+    timer = updateTimer();
+    ifOngoing = true;
+    runQuestions();
     // call form function
-});
+}
+
+document.getElementById('start').addEventListener("click", startQuiz);
+
+document.getElementById('high-score').addEventListener('click', viewHighScores);
